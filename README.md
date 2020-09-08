8ad93adf1c0ee30bd70ebc29a81627e7195e6c6e
Trinnawat <trinnawat_got@hotmail.com>
2020-09-08 18:05:49 +0700

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# For Dev Env
+
+- Please install skaffold <https://skaffold.dev/>
+- Add '127.0.0.1 posts.com' at C:\Windows\System32\drivers\etc\hosts (Windows)
---
+++ client/src/CommentList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default ({ comments }) => {
+    const renderedComments = comments.map(comment => {
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content;
+        } else if (comment.status === 'pending') {
+            content = 'This commnet is awaiting moderation';
+        } else if (comment.status === 'rejected') {
+            content = 'This comment has been rejected';
+        } 
+        return <li key={comment.id}>{content}</li>;
+    });
+    return <ul>{renderedComments}</ul>;
+};
